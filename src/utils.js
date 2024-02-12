fbba556a34c43edf2830ee848cdc39123ecddc09
Luis Miguel <luibelzan@outlook.com>
2024-02-12 11:17:35 +0100

    S63 and S65 parsed

--- src/utils.js
+++ src/utils.js
@@ -27,21 +27,70 @@ function parseXML(xmlString) {
                         var fh = result.Report.Cnc[0].Cnt[0].S13[0].$.Fh;
                         var et = result.Report.Cnc[0].Cnt[0].S13[0].$.Et;
                         var c = result.Report.Cnc[0].Cnt[0].S13[0].$.C;
+                        var d1 = result.Report.Cnc[0].Cnt[0].S13[0].$.D1;
+                        var d2 = result.Report.Cnc[0].Cnt[0].S13[0].$.D2;
+                        var errCat = result.Report.Cnc[0].Cnt[0].S13[0].$.ErrCat;
+                        var errCode = result.Report.Cnc[0].Cnt[0].S13[0].$.ErrCode;
 
-                        // Imprimir los valores obtenidos
-                        console.log(idRpt);
-                        console.log(idPet);
-                        console.log(version);
                         console.log(cncId);
                         console.log(cntId);
                         console.log(fh);
                         console.log(et);
                         console.log(c);
+                        console.log(d1);
+                        console.log(d2);
+                        console.log(errCat);
+                        console.log(errCode);
+                    } else if(idRpt == 'S15') {
+                        var cncId = result.Report.Cnc[0].$.Id;
+                        var fh = result.Report.Cnc[0].S15[0].$.Fh;
+                        var et = result.Report.Cnc[0].S15[0].$.Et;
+                        var c = result.Report.Cnc[0].S15[0].$.C;
+                        var d1 = result.Report.Cnc[0].S15[0].$.D1;
+                        var d2 = result.Report.Cnc[0].S15[0].$.D2;;
 
+                        console.log(cncId);
+                        console.log(fh);
+                        console.log(et);
+                        console.log(c);
+                        console.log(d1);
+                        console.log(d2);
                     } else if(idRpt == 'S31') {
-                        
+                        var cncId = result.Report.Cnc[0].$.Id;
+                        var cntId = result.Report.Cnc[0].Cnt[0].$.Id;
+                        var fh = result.Report.Cnc[0].Cnt[0].S31[0].$.Fh;
+                        var clientId = result.Report.Cnc[0].Cnt[0].S31[0].$.ClientId;
+                        var status = result.Report.Cnc[0].Cnt[0].S31[0].$.Status;
+                        var keyRequest = result.Report.Cnc[0].Cnt[0].S31[0].$.KeyRequest;
+
+                        console.log(cncId);
+                        console.log(cntId);
+                        console.log(fh);
+                        console.log(clientId);
+                        console.log(status);
+                        console.log(keyRequest);
+                    } else if(idRpt == 'S63') {
+                        var rtu = result.Report.Rtu[0].$.Id;
+                        var lvsid = result.Report.Rtu[0].LVSLine[0].$.Id;
+                        var lvspos = result.Report.Rtu[0].LVSLine[0].$.Pos;
+                        var errCat = result.Report.Rtu[0].LVSLine[0].$.ErrCat;
+                        var errCode = result.Report.Rtu[0].LVSLine[0].$.ErrCode;
+                        var fh = result.Report.Rtu[0].LVSLine[0].S63[0].$.Fh;
+                        var et = result.Report.Rtu[0].LVSLine[0].S63[0].$.Et;
+                        var c = result.Report.Rtu[0].LVSLine[0].S63[0].$.C;
+                        var d1 = result.Report.Rtu[0].LVSLine[0].S63[0].$.D1;
+                        var d2 = result.Report.Rtu[0].LVSLine[0].S63[0].$.D2;
+
+                    } else if(idRpt == 'S65') {
+                        var rtu = result.Report.Rtu[0].$.Id;
+                        var errCat = result.Report.Rtu[0].$.ErrCat;
+                        var errCode = result.Report.Rtu[0].$.ErrCode;
+                        var fh = result.Report.Rtu[0].S65[0].$.Fh;
+                        var et = result.Report.Rtu[0].S65[0].$.Et;
+                        var c = result.Report.Rtu[0].S65[0].$.C;
+                        var d1 = result.Report.Rtu[0].S65[0].$.D1;
+                        var d2 = result.Report.Rtu[0].S65[0].$.D2;
                     }
-                    var clientId = result.Report.Cnc[0].Cnt[0].S13[0].$.ClientId;
                 }
             });
         }
